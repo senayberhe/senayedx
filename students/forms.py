6d956a96c474f30b85972b000b34b9241c73eb0b
senayberhe <tsionberhe@gmail.com>
2022-06-06 09:58:09 -0700

    created chat app and also channels

--- students/forms.py
+++ students/forms.py
@@ -1,7 +1,7 @@
-from django import forms 
-from courses.models import Course 
+from django import forms
+from courses.models import Course
+
 
 class CourseEnrollForm(forms.Form):
     course = forms.ModelChoiceField(queryset=Course.objects.all(),
-            widget=forms.HiddenInput)
-
+                                    widget=forms.HiddenInput)
